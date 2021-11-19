ecb00094dfd1b35ffaf74b71ee96ac1f7106c4d6
Andrea Foletto <andrewfoll@protonmail.com>
2021-11-19 12:55:33 +0100

    initial commit, first version of the script

---
+++ version.sh
@@ -0,0 +1,52 @@
+
+#!/usr/bin/env sh
+# author: Andrea Foletto @nullndr
+
+set -o pipefail
+set -u
+
+usage() {
+  echo "Usage: $0 [command]"
+}
+
+print_version() {
+  echo "$0 v0.1"
+  echo "This is a free software; There is NO warranty."
+  echo "See the source for more."
+}
+
+main() {
+  
+  if [[ $# -eq 0 || $# -gt 1 ]]; then
+    usage $@
+    exit 1
+  fi
+
+  if [[ $1 == "--version" ]]; then
+    print_version $0
+    exit 0
+  fi
+  
+  $1 > /dev/null 2>&1
+
+  if [ $? -eq 127 ]; then
+    echo "$0: command not found: $1"
+  else
+    
+    echo "$1 --version | read output"
+    $1 --version | read output
+    echo $output    
+
+ 
+    if [ $? -ne 0 ]; then
+      echo "Command $1 does not have a '--version' flag"
+      exit 0
+    fi
+
+    echo $output
+  fi
+  
+  exit 0
+}
+
+main $@
