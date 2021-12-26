27e39aa4df7ac95e13d23b5bbde1fdb03843c7cd
Andrea <andrewfoll@protonmail.com>
2021-12-26 19:05:25 +0100

    Code refactoring

--- version.sh
+++ version.sh
@@ -1,36 +1,43 @@
 #!/usr/bin/env bash
-# author: @nullndr
+# author: @nullndr <andrea@yaaaw.it>
 
 set -o pipefail
 set -u
 
-usage() {
-  echo "Usage: version [[command] [options]]"
+print_usage() {
+  echo "Usage: version <command>"
+  echo "       version <option> "
   echo "where Options:"
   echo -e "\t--version: print software's version"
   echo -e "\t--help, -h: print this page"
 }
 
 print_version() {
-  echo "version v0.1"
+  version="0.1"
+  echo "version $version"
   echo "This is a free software; There is NO warranty."
   echo "See the source for more."
 }
 
+check_args() {
+
+  if [[ $# -ne 1  ]]; then
+    print_usage
+    exit 128
+  fi
+
+  case $1 in
+   	'--help' | '-h' ) print_usage; exit 0 ;;
+  	'--version' ) print_version; exit 0 ;;
+  esac
+}
+
 main() {
 
-  if [[ $# -lt 1 || $# -gt 2 || $1 == "--help" || $1 == "-h" ]]; then
-    usage
-    exit 1
-  fi
-
-  if [[ $1 == "--version" ]]; then
-    print_version
-    exit 0
-  fi
+  check_args "$@"
 
   if ! which "$1" > /dev/null 2> /dev/null ; then
-    echo "version: command not found: $1"
+    echo "version: command $1 not found"
   else
 
 		first_flag=1
@@ -41,13 +48,15 @@ main() {
 
     if [[ $first_flag -eq 0 ]] && [[ $second_flag -eq 0 ]]; then
     	echo "Command $1 does not have a --version or -version flag :("
-    	exit 0
+    else
+      if [[ $first_flag -eq 0 ]]; then
+        $1 --version
+      elif [[ $second_flag -eq 0 ]]; then
+        $1 -version
+      fi
   	fi
-
-    if [[ $first_flag -eq 0 ]]; then $1 --version; exit 0; fi
-    if [[ $second_flag -eq 0 ]]; then $1 -version; exit 0; fi
   fi
-  
+
   exit 0
 }
 
